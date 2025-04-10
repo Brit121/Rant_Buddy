dca3c59a4cf4069bbf38bc991c8aaa6288665dd1
VERSOZA <taniahjaneversoza@gmail.com>
2025-04-11 00:50:05 +0800

    semi-update

--- Program.cs
+++ Program.cs
@@ -4,30 +4,31 @@ using RantBuddyBusinessDataLogic;
 
 namespace rantBuddy
 
+
 {
     internal class Program
     {
-        static string[] actions = new string[] { "[1] Add an Entry", "[2] View Entries", "[3] Delete an Entry", "[4] Exit" };
+        static string[] actions = new string[] { "[1] Create or Add an Entry", "[2] Retrieve or View Entries", "[3] Update an Entry", "[4] Delete an Entry", "[5] Exit" };
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Welcome to your Rant Buddy!");
             //passcode, add entry, view entries, delete entries, exit
 
-            int passcode = 1201;
+            int pin = 1201;
             int password;
 
 
             while (true)
             {
-                Console.Write("\nPlease enter your 4-digit passcode: \n");
+                Console.Write("\nPlease enter your 4-digit pin: \n");
                 string input = Console.ReadLine();
 
-                if (int.TryParse(input, out password) && password == passcode)
+                if (int.TryParse(input, out password) && password == pin)
                 {
                     Console.WriteLine("\n------------- Logged In Successfully! Welcome! --------------");
                     break;
                 }
-                Console.WriteLine("\n------------- Incorrect Passcode! Use four numbers only.--------------");
+                Console.WriteLine("\n------------- Incorrect Pin! Please use four numbers only.--------------");
             }
             int userOption;
 
@@ -41,28 +42,31 @@ namespace rantBuddy
                     switch (userOption)
                     {
                         case 1:
-                            AddEntry();
+                            CreateOrAddEntry();
                             break;
                         case 2:
-                            ViewEntries();
+                            RetrieveOrViewEntries();
                             break;
                         case 3:
-                            DeleteEntry();
+                            UpdateEntry();
                             break;
                         case 4:
+                            DeleteEntry();
+                            break;
+                        case 5:
                             Console.WriteLine("\n------------ Sayonara!!!!!!!!! ------------");
                             break;
                     }
                 }
             }
 
-            while (userOption != 4);
+            while (userOption != 5);
 
         }
 
 
         static void DisplayActions()
-        { 
+        {
             Console.WriteLine("\nPlease choose an option: ");
             foreach (string action in actions)
             {
@@ -76,7 +80,7 @@ namespace rantBuddy
             {
                 Console.Write("\n[User Option]:  \n");
                 string input = Console.ReadLine();
-                if (int.TryParse(input, out option) && option >= 1 && option <= 4)
+                if (int.TryParse(input, out option) && option >= 1 && option <= 5)
                 {
                     return option;
                 }
@@ -84,7 +88,7 @@ namespace rantBuddy
                 Console.Write("\n------------------------------------------------------------------------------");
             }
         }
-        static void AddEntry()
+        static void CreateOrAddEntry()
         {
             Console.WriteLine("\nAdd an entry: \n");
             string entry = Console.ReadLine();
@@ -98,7 +102,7 @@ namespace rantBuddy
                 Console.Write("\nNo Entry Provided. Please Enter an Entry.\n");
             }
         }
-        static void ViewEntries()
+        static void RetrieveOrViewEntries()
         {
             bool result = RantBuddyEntries.HasEntries();
             {
@@ -115,6 +119,43 @@ namespace rantBuddy
                         Console.WriteLine("\n------------------------------------------------");
                     }
                 }
+            }
+        }
+        static void UpdateEntry()
+        {
+            if (RantBuddyEntries.rantEntries.Count == 0)
+            {
+                Console.WriteLine("\n--------------- No Entries to update. --------------");
+                Console.WriteLine("\n-----------------------------------------------------");
+
+            }
+            Console.WriteLine("\nEntries: ");
+            for (int i = 0; i < RantBuddyEntries.rantEntries.Count; i++)
+            {
+                Console.WriteLine($"\n{i + 1}. {RantBuddyEntries.rantEntries[i]}");
+                Console.WriteLine("\n------------------------------------------------");
+            }
+            Console.WriteLine("\nPlease enter the number of the entry you want to update:  ");
+            if (int.TryParse(Console.ReadLine(), out int index) && index > 0 && index <= RantBuddyEntries.rantEntries.Count)
+            {
+                Console.WriteLine("\nPlease enter the new entry: ");
+                string newEntry = Console.ReadLine();
+                if (!string.IsNullOrEmpty(newEntry))
+                {
+                    RantBuddyEntries.rantEntries[index - 1] = newEntry;
+                    Console.WriteLine("\n--------------- Entry updated successfully! --------------------");
+                    Console.WriteLine("\n------------------------------------------------------------------");
+                }
+                else
+                {
+                    Console.WriteLine("\n--------- Invalid Entry! Please Try Again. ----------");
+                    Console.WriteLine("\n--------------------------------------------------");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\n--------- Invalid choice of number! Please Try Again. ----------");
+                Console.WriteLine("\n--------------------------------------------------");
             }
         }
         static void DeleteEntry()
